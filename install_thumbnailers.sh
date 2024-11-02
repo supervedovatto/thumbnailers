9ed73692abdcbe589bda7e2bc42fe62f312a4fcf
supervedovatto <thiagovedovatto@gmail.com.br>
2024-11-02 10:21:14 -0300

    Commit inicial

---
+++ install_thumbnailers.sh
@@ -0,0 +1,7 @@
+cp *.thumbnailer /usr/share/thumbnailers
+sudo apt install ddjvu -y
+sudo apt install evince-thumbnailer -y
+sudo apt install ffmpegthumbnailer -y
+sudo apt install gdk-pixbuf-thumbnailer -y
+sudo apt install gnome-thumbnail-font -y
+sudo apt install gm -y
